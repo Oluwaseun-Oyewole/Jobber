7407f47d1a8177bb78617cbc75d3ee6ddc4a8f05
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-04-04 19:42:45 +0100

    prettier, eslint, husky setups

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// .eslintrc.cjs
+/* eslint-env node */
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+// "test-all": "npm run check-format && npm run check-lint && npm run check-types && npm run build",
