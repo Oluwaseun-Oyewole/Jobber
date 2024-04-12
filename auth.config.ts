b5433aeed85a3c7bcbc2b369f4927a8d0150f9b1
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-04-12 06:41:28 +0100

    resolve db timeout

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,6 @@ import type { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import LinkedInProvider from "next-auth/providers/linkedin";
 import { getUserByEmail } from "./lib/query";
 import { loginValidationSchema } from "./lib/schema/login";
 
@@ -19,37 +18,37 @@ export default {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
 
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_SECRET,
-      redirectProxyUrl: "http://localhost:3000",
-      authorization: {
-        url: "https://www.linkedin.com/oauth/v2/authorization",
-        params: {
-          scope: "profile email openid",
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-      issuer: "https://www.linkedin.com",
-      userinfo: {
-        url: "https://api.linkedin.com/v2/userinfo",
-      },
-      token: {
-        url: "https://www.linkedin.com/oauth/v2/accessToken",
-      },
-      jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
-      async profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          firstname: profile.given_name,
-          lastname: profile.family_name,
-          email: profile.email,
-        };
-      },
-    }),
+    // LinkedInProvider({
+    //   clientId: process.env.LINKEDIN_CLIENT_ID,
+    //   clientSecret: process.env.LINKEDIN_SECRET,
+    //   redirectProxyUrl: "http://localhost:3000",
+    //   authorization: {
+    //     url: "https://www.linkedin.com/oauth/v2/authorization",
+    //     params: {
+    //       scope: "profile email openid",
+    //       prompt: "consent",
+    //       access_type: "offline",
+    //       response_type: "code",
+    //     },
+    //   },
+    //   issuer: "https://www.linkedin.com",
+    //   userinfo: {
+    //     url: "https://api.linkedin.com/v2/userinfo",
+    //   },
+    //   token: {
+    //     url: "https://www.linkedin.com/oauth/v2/accessToken",
+    //   },
+    //   jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
+    //   async profile(profile) {
+    //     return {
+    //       id: profile.sub,
+    //       name: profile.name,
+    //       firstname: profile.given_name,
+    //       lastname: profile.family_name,
+    //       email: profile.email,
+    //     };
+    //   },
+    // }),
 
     CredentialsProvider({
       name: "Credentials",
