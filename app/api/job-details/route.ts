6ed7cabb5ea3c126de833dd8228c620ba79f3cc6
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-04-23 06:44:55 +0100

    vercel prod fix

--- app/api/job-details/route.ts
+++ app/api/job-details/route.ts
@@ -1,3 +1,6 @@
+export const maxDuration = 10;
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
